5d590ee3f6f392cf7473ba84f697440af8cebaa3
fatima_dy <fatimady.FKD@gmail.com>
2024-03-26 22:08:28 +0800

    worked on reflection and resume, edited index

--- index.js
+++ index.js
@@ -5,4 +5,24 @@ function changeColor(element) {
     }
     
     element.classList.add('active');
-    }
+}
+
+function highlightNavLink() {
+    var sections = document.querySelectorAll('.content-section');
+    var navOptions = document.getElementsByClassName('nav-option');
+
+    sections.forEach(function(section, index) {
+        var rect = section.getBoundingClientRect();
+
+        if (rect.top <= 0 && rect.bottom > 0) {
+            for (var i = 0; i < navOptions.length; i++) {
+                navOptions[i].classList.remove('active');
+            }
+            navOptions[index].classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', function() {
+    highlightNavLink();
+});
